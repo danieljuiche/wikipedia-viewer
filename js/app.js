e163076d161539c3b787913a6b433dd5e1514a7c
Daniel <captainmunie@gmail.com>
2017-02-13 13:55:47 -0800

    Set up call to wikipedia using Angular

--- js/app.js
+++ js/app.js
@@ -5,14 +5,46 @@ var wikipediaApp = angular.module('wikipediaApp', []);
 wikipediaApp.config(function ($sceDelegateProvider) {
 
 	// Whitelist wikipedia API
-	$sceDelegateProvider.resourceUrlWhitelist(['self', 'http://en.wikipedia.org/w/api.php?action=opensearch&search=testing&limit=10&namespace=0']);
+	$sceDelegateProvider.resourceUrlWhitelist(['self', 'http://en.wikipedia.org/w/api.php?']);
 
 });
 
-// Set up main controller
-wikipediaApp.controller('mainController', ['$http', function ($http) {
+// Service
+wikipediaApp.service('wikipediaService', ['$scope', '$http', function ($scope, $http) {
+	var queryUrl = 'http://en.wikipedia.org/w/api.php?';
 
 	// Testing API call to wikipedia
-	console.log($http.jsonp('http://en.wikipedia.org/w/api.php?action=opensearch&search=testing&limit=10&namespace=0', {jsonpCallbackParam: 'callback'}));
+	$http.jsonp(queryUrl, {
+		jsonpCallbackParam: 'callback',
+		params: {
+			action: 'opensearch',
+			search: 'testing',
+			limit: 10,
+			namespace: 0
+		}
+	})	
+	.then(function(data){
+		console.log(data);
+	});
+}]);
+
+// Set up main controller
+wikipediaApp.controller('mainController', ['$http', 'wikipediaService', function ($http, wikipediaService) {
+
+	/*var queryUrl = 'http://en.wikipedia.org/w/api.php?';
+
+	// Testing API call to wikipedia
+	$http.jsonp(queryUrl, {
+		jsonpCallbackParam: 'callback',
+		params: {
+			action: 'opensearch',
+			search: 'testing',
+			limit: 10,
+			namespace: 0
+		}
+	})	
+	.then(function(data){
+		console.log(data);
+	});*/
 
 }]);
