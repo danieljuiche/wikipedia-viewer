93fc77c9652f4d8c846a9c4c415d60d920f63743
Daniel <captainmunie@gmail.com>
2017-02-15 16:20:41 -0800

    Set up error handling for API call

--- js/app.js
+++ js/app.js
@@ -5,33 +5,48 @@ var wikipediaApp = angular.module('wikipediaApp', []);
 wikipediaApp.config(function ($sceDelegateProvider) {
 
 	// Whitelist wikipedia API
-	$sceDelegateProvider.resourceUrlWhitelist(['self', 'http://en.wikipedia.org/w/api.php?']);
-
+	$sceDelegateProvider.resourceUrlWhitelist(['self', 'http://en.wikipedia.org/w/api.php?', 'https://api.github.com/events?']);
 });
 
-// Service - http://stackoverflow.com/questions/39203472/angularjs-service-call-api-and-return-json
-//         - http://kirkbushell.me/when-to-use-directives-controllers-or-services-in-angular
-wikipediaApp.service('wikipediaService', ['$http', function ($http) {
+// Service
+wikipediaApp.service('wikipediaService', ['$http', '$q', function ($http, $q) {
 	var queryUrl = 'http://en.wikipedia.org/w/api.php?';
-
-	// Testing API call to wikipedia 
-	$http.jsonp(queryUrl, {
-		jsonpCallbackParam: 'callback',
-		params: {
-			action: 'opensearch',
-			search: 'testing',
-			limit: 10,
-			namespace: 0
+	return {
+		getData: function () {
+			return $http.jsonp(queryUrl, {
+				jsonpCallbackParam: 'callback',
+				params: {
+					action: 'opensearch',
+					search: 'Vancouver',
+					limit: 10,
+					namespace: 0
+				}
+			}).then(function (response) {
+				if (typeof response === 'object') {
+					return response;
+				}
+				else {
+					// Something went wrong
+					return $q.reject(response);
+				}
+			}, function (response) {
+				// Something went wrong
+				return $q.reject(response);
+			});
 		}
-	})	
-	.then(function(data){
-		console.log(data);
-		return data;
-	});
+	}
 }]);
 
 // Set up main controller
-wikipediaApp.controller('mainController', ['$scope', '$http', 'wikipediaService', function ($scope, $http, wikipediaService) {
-	console.log(wikipediaService);
-
+wikipediaApp.controller('mainController', ['$scope', 'wikipediaService', function ($scope, wikipediaService) {
+	wikipediaService.getData().then(function (data) {
+		if (data.status === 200) {
+			$scope.dataReturned = data;
+		} else {
+			$scope.dataReturned = "Something went wrong!";
+		}
+	}, function (error) {
+		// Something went wrong
+		console.log(error);
+	});
 }]);
