e395203df0e09acaf113009caaac738d41cb2e04
Daniel <captainmunie@gmail.com>
2017-02-16 19:53:29 -0800

    Basic search functionality

--- js/app.js
+++ js/app.js
@@ -5,19 +5,19 @@ var wikipediaApp = angular.module('wikipediaApp', []);
 wikipediaApp.config(function ($sceDelegateProvider) {
 
 	// Whitelist wikipedia API
-	$sceDelegateProvider.resourceUrlWhitelist(['self', 'http://en.wikipedia.org/w/api.php?', 'https://api.github.com/events?']);
+	$sceDelegateProvider.resourceUrlWhitelist(['self', 'http://en.wikipedia.org/w/api.php?']);
 });
 
 // Service
 wikipediaApp.service('wikipediaService', ['$http', '$q', function ($http, $q) {
 	var queryUrl = 'http://en.wikipedia.org/w/api.php?';
 	return {
-		getData: function () {
+		getData: function (queryValue) {
 			return $http.jsonp(queryUrl, {
 				jsonpCallbackParam: 'callback',
 				params: {
 					action: 'opensearch',
-					search: 'Vancouver',
+					search: queryValue,
 					limit: 10,
 					namespace: 0
 				}
@@ -39,14 +39,42 @@ wikipediaApp.service('wikipediaService', ['$http', '$q', function ($http, $q) {
 
 // Set up main controller
 wikipediaApp.controller('mainController', ['$scope', 'wikipediaService', function ($scope, wikipediaService) {
-	wikipediaService.getData().then(function (data) {
-		if (data.status === 200) {
-			$scope.dataReturned = data;
-		} else {
-			$scope.dataReturned = "Something went wrong!";
-		}
-	}, function (error) {
-		// Something went wrong
-		console.log(error);
-	});
+	$scope.searchQuery = '';
+	$scope.results = [];
+	
+	$scope.search = function () {
+			wikipediaService.getData($scope.searchQuery).then(function (data) {
+			if (data.status === 200) {
+				// API call success
+				$scope.dataReturned = data;
+
+				// Console data logging
+				console.log(data);
+				$scope.results = [];
+				for (var i = 0; i < data.data[1].length; i++) {
+					$scope.results.push({
+						resultID: i,
+						title: data.data[1][i],
+						description: data.data[2][i],
+						url: data.data[3][i]
+					});
+				}
+				console.log($scope.results);
+
+			} else {
+				$scope.dataReturned = "Something went wrong!";
+			}
+		}, function (error) {
+			// Something went wrong
+			console.log(error);
+		});
+	}
 }]);
+
+// Directives
+wikipediaApp.directive('searchResults', function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'directives/searchResults.html'
+	}
+});
